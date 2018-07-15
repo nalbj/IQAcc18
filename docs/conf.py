3b727eeae055c7680d1e07306cb80dd9efd90087
Jeff Nalbone <j.nalbone@f5.com>
2018-07-15 15:03:46 -0400

    conf-py commented out more f5 theme

--- docs/conf.py
+++ docs/conf.py
@@ -77,8 +77,8 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = 'f5_sphinx_theme'
-html_theme_path = f5_sphinx_theme.get_html_theme_path()
+#html_theme = 'f5_sphinx_theme'
+#html_theme_path = f5_sphinx_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
